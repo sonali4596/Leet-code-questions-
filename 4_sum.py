a2d50e643338005dfbb11802f8f790806a52e8b5
sonali4596 <sonalianmol4596@gmail.com>
2021-08-08 18:31:12 +0530

    Update 4_sum.py

--- 4_sum.py
+++ 4_sum.py
@@ -25,6 +25,102 @@ Constraints:
 -109 <= target <= 109
 question link: https://leetcode.com/problems/4sum/
 '''
+#the write solution
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        front=0
+        back=0
+        nums.sort()
+        #print(nums)
+        ans=[]
+        inci=incj=1
+        
+        def dodge_duplicate(n:int):
+            j=0
+            for p in range(n+1,len(nums)):
+                if(nums[n+1]==nums[p]):
+                    j+=1
+                else:
+                    break;
+            #print("dodjing value",j)
+            return j
+        def dodge_duplicate_back(n:int):
+            j=0
+            for p in range(n-1,0,-1):
+                if(nums[n]==nums[p]):
+                    j+=1
+                else:
+                    break;
+            #print("dodjing value back",j)
+            return j
+       
+        i=0
+        while i<len(nums)-1:
+            #print("i=",i)
+            j=i+1
+            while j<(len(nums)-1):
+                #print("j=",j)
+                n=target-nums[i]-nums[j]
+                #print("n=",n)
+                front=j+1
+                #print("front=",front)
+                back=len(nums)-1
+                #print("back=",back)
+                while(front<back):
+                    if(nums[front]+nums[back])<n:
+                        #print("front before",front)
+                        if(nums[front]==nums[front+1]):
+                            front+=dodge_duplicate(front)+1
+                        else:
+                            front+=1
+                        #print("front after",front)
+                        
+                       
+                       
+                        
+                        
+                        
+                    elif(nums[front]+nums[back])>n:
+                        #print("back before",back)
+                        if(nums[back]==nums[back-1]):
+                            back-=dodge_duplicate_back(back)+1
+                        else:
+                            back-=1
+                        #print("back after ",back)
+                        
+                    else:
+                        ans.append([nums[i],nums[j],nums[front],nums[back]])
+                        if(nums[front]==nums[front+1]):
+                            front+=dodge_duplicate(front)+1
+                        else:
+                            front+=1
+                        if(nums[back]==nums[back-1]):
+                            back-=dodge_duplicate_back(back)+1
+                        else:
+                            back-=1
+                        #print("ans list ",ans)
+                        #print("front",front)
+                        #print("back",back)
+                if(nums[j]==nums[j+1]):
+                    j+=dodge_duplicate(j)+1
+                    
+                    #print("incj",j)
+                else:
+                     j=j+1
+            if(nums[i]==nums[i+1]):
+                    i+=dodge_duplicate(i)+1
+                   
+                    #print("inci",i)
+            else:
+                     i=i+1
+        #print("answer list finally",ans)
+        return ans 
+       
+       
+       
+       
+       
+#-------------------------------------------------------------------------------------------------------------------------       
 #this solution gives memory exceeded
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
